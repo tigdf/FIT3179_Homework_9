c9e6237ab73b1c25f6078fbbe6483d868726251a
tigdf <58255157+tigdf@users.noreply.github.com>
2021-09-26 21:58:18 +1000

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var hw_1 = "choropleth-map.vg.json";
+var hw_1 = "choropleth_map.vg.json";
 vegaEmbed("#choropleth_map", hw_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
